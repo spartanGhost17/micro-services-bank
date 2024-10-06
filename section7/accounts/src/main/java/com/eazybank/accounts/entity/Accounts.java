e9ccc6661b8650f2ca8e5eceacec7eddef398433
spartanGhost17 <adambaku5216@gmail.com>
2024-10-06 14:30:07 +0100

    changed h2 in memory db to mysql db

---
+++ section7/accounts/src/main/java/com/eazybank/accounts/entity/Accounts.java
@@ -0,0 +1,24 @@
+package com.eazybank.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Accounts extends BaseEntity{
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_number")
+    private Long accountNumber;
+    @Column(name = "customer_id")
+    private Long customerId;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
---
+++ section7/accounts/src/main/java/com/eazybank/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.eazybank.accounts.mapper;
+
+import com.eazybank.accounts.dto.CustomerDto;
+import com.eazybank.accounts.entity.Customer;
+import org.springframework.beans.BeanUtils;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        BeanUtils.copyProperties(customer, customerDto);
+        //customerDto.setName(customer.getName());
+        //customerDto.setEmail(customer.getEmail());
+        //customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }}
