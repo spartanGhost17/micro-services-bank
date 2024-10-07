87ae7ae26fe487d029bca1b7f5d2d06781c97235
spartanGhost17 <adambaku5216@gmail.com>
2024-10-07 19:46:08 +0100

    experimenting with resiliency pattern sanbox 10

---
+++ section10/accounts/src/main/java/com/eazybank/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,18 @@
+package com.eazybank.accounts.service.client;
+
+import com.eazybank.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static com.eazybank.accounts.constants.AccountsConstants.EAZYBANK_CORRELATION_ID;
+
+@FeignClient(name = "cards")
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader(EAZYBANK_CORRELATION_ID) String correlationId,
+                                                     @RequestParam String mobileNumber);
+}
