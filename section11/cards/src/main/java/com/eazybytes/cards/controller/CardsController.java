e226d8db1e2a4083233b54b4b1e9d4b2596285c3
spartanGhost17 <adambaku5216@gmail.com>
2024-10-08 22:29:40 +0100

    added open telemtry tracing with tempo backend, grafana dashboards, loki, alloy & prometheus for metrics

--- section11/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ section11/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -108,8 +108,9 @@ public class CardsController {
                                                         @RequestParam
                                                         @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                         String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {}", correlationId);
+        logger.debug("fetchCardDetails method start"); //open telemetry will attach distributed tracing
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        logger.debug("fetchCardDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
